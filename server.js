d75b0a60f2911caebb5a41a39fb148564438c273
Matt Byrnes <mattjbyrnes@gmail.com>
2018-09-25 14:56:47 -0400

    PORT Fix for Heroku

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 4000;
+// const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
